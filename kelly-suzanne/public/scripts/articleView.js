a7b3bf7addfb9c9262468f93547e542c825e329f
Kelly Bennett <cornerkick87@gmail.com>
2018-06-13 11:26:46 -0700

    read through reviews and comments

--- kelly-suzanne/public/scripts/articleView.js
+++ kelly-suzanne/public/scripts/articleView.js
@@ -3,7 +3,7 @@
 const articleView = {};
 
 articleView.populateFilters = () => {
-  $('article').each(function() {
+  $('article').each(function () {
     if (!$(this).hasClass('template')) {
       let val = $(this).find('address a').text();
       let optionTag = `<option value="${val}">${val}</option>`;
@@ -22,7 +22,7 @@ articleView.populateFilters = () => {
 };
 
 articleView.handleAuthorFilter = () => {
-  $('#author-filter').on('change', function() {
+  $('#author-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-author="${$(this).val()}"]`).fadeIn();
@@ -35,7 +35,7 @@ articleView.handleAuthorFilter = () => {
 };
 
 articleView.handleCategoryFilter = () => {
-  $('#category-filter').on('change', function() {
+  $('#category-filter').on('change', function () {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-category="${$(this).val()}"]`).fadeIn();
@@ -48,7 +48,7 @@ articleView.handleCategoryFilter = () => {
 };
 
 articleView.handleMainNav = () => {
-  $('nav').on('click', '.tab', function(e) {
+  $('nav').on('click', '.tab', function (e) {
     e.preventDefault();
     $('.tab-content').hide();
     $(`#${$(this).data('content')}`).fadeIn();
@@ -59,7 +59,7 @@ articleView.handleMainNav = () => {
 
 articleView.setTeasers = () => {
   $('.article-body *:nth-of-type(n+2)').hide();
-  $('article').on('click', 'a.read-on', function(e) {
+  $('article').on('click', 'a.read-on', function (e) {
     e.preventDefault();
     if ($(this).text() === 'Read on →') {
       $(this).parent().find('*').fadeIn();
@@ -67,19 +67,19 @@ articleView.setTeasers = () => {
     } else {
       $('body').animate({
         scrollTop: ($(this).parent().offset().top)
-      },200);
+      }, 200);
       $(this).html('Read on &rarr;');
       $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
     }
   });
 };
 
-// COMMENT: When/where is this function invoked? What event ultimately triggers its execution? Explain the sequence of code execution when this function is invoked.
-// PUT YOUR RESPONSE HERE
+// DONE COMMENT: When/where is this function invoked? What event ultimately triggers its execution? Explain the sequence of code execution when this function is invoked.
+// RESPONSE: The function is invoked on load of the new.html page. Calls on articleView.create which creates the article instance and appends it to the DOM. Then it calls on articleView.submit, which calls on article.insertRecord, which refers to Article.prototype.insertRecord, which calls a callback function.
 articleView.initNewArticlePage = () => {
   $('.tab-content').show();
   $('#export-field').hide();
-  $('#article-json').on('focus', function(){
+  $('#article-json').on('focus', function () {
     this.select();
   });
 
@@ -104,7 +104,7 @@ articleView.create = () => {
 
   $('#articles').append(article.toHtml());
 
-  $('pre code').each(function(i, block) {
+  $('pre code').each(function (i, block) {
     hljs.highlightBlock(block);
   });
 
@@ -131,7 +131,7 @@ articleView.submit = event => {
 }
 
 articleView.initIndexPage = () => {
-  Article.all.forEach(article =>{
+  Article.all.forEach(article => {
     $('#articles').append(article.toHtml())
   });
 
